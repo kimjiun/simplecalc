ba66ad925d1453eebebb3bcdec9bbfbca56396a9
kimjiun <ggkimh@hanmail.net>
2021-10-11 09:48:58 +0900

    gradle 세팅

--- app/src/main/java/kim/jiun/simplecalc/MainActivity.kt
+++ app/src/main/java/kim/jiun/simplecalc/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inits()
+    }
+
+    fun inits() : Unit{
+        var tmpList : MutableList<Any> = mutableListOf("hello")
+        tmpList.add(1)
     }
 }
